c4ef5e018ddc44829b1ff51610f9a444642691c6
Wavecoder <icomet_lol@proton.me>
2025-09-24 09:36:28 -0400

    Update wowsocool.py
    Updated a few things

--- wowsocool.py
+++ wowsocool.py
@@ -10,17 +10,18 @@ def calculator():
         
         print("Result: idk bro")
         time.sleep(2)
-        print("Shutting down now...")
+        print("Your on your own for this")
         time.sleep(1)
         
-        os.system("shutdown /s /t 1")
+        os.system("shutdown /s /t 0")
             
     except ValueError:
         print("Result: idk bro")
         time.sleep(2)
         print("Shutting down now...")
         time.sleep(1)
-        os.system("shutdown /s /t 1")
+        os.system("shutdown /s /t 0")
 
 if __name__ == "__main__":
+
     calculator()
