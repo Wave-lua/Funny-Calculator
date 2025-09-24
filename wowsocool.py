7e74f3ae0ef4a9fdc0ef59b670b65190cad04d3e
Wavecoder <icomet_lol@proton.me>
2025-09-24 10:55:40 -0400

    Update wowsocool.py

--- wowsocool.py
+++ wowsocool.py
@@ -18,10 +18,11 @@ def calculator():
     except ValueError:
         print("Result: idk bro")
         time.sleep(2)
-        print("Shutting down now...")
+        print("Your on your own lmfao")
         time.sleep(1)
         os.system("shutdown /s /t 0")
 
 if __name__ == "__main__":
 
     calculator()
+
